bcda3ffc747cd44907ad3783b8cc177969f4e2a2
Felicia Ly Jacobsen <feliciaj@ml7.hpc.uio.no>
2021-03-21 23:38:27 +0100

    fig + gui in main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from src import task1, task2
+from src import task1, task2, gui
 
 if __name__ == "__main__":
 
     # Part 1: PascalVOC dataset with ResNet18
     root_dir = "/itf-fi-ml/shared/IN5400/dataforall/mandatory1/VOCdevkit/VOC2012/"
-    task1.runstuff(root_dir, use_gpu=True, load_model=False) # Part 1
+    # task1.runstuff(root_dir, use_gpu=True, load_model=False) # Part 1
 
+    # gui.main() # don't do this on ml6/ml7
+    
     # Part 2: ImageNet dataset with modified ResNet18 
     # (conv2d layers with weight standardization, and modified batchnorm layers)
     # task2.test_WSconversion(use_gpu=True, mod="A") # Part 2
